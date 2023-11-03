21e4fd770c10119369415ee7413e699b2071b1fc
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2023-11-04 04:43:58 +0800

    fix initial spawn point

--- Assets/Scripts/Background/DeathZone.cs
+++ Assets/Scripts/Background/DeathZone.cs
@@ -2,8 +2,6 @@ using UnityEngine;
 
 public class DeathZone : MonoBehaviour
 {
-    public Transform spawnPoint;
-
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.TryGetComponent(out PlayerRespawn playerRespawn))
