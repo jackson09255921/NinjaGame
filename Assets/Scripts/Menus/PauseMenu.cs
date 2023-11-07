cfd97c16374149438b439e9c6a778f267456097f
Wang Guan-Jie <jackson09255921@gmail.com>
2023-11-07 10:33:38 +0800

    Start Scene Rule

--- Assets/Scripts/Menus/PauseMenu.cs
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -3,4 +3,10 @@ using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
+    public SceneTransition sceneTransitionPrefab;
+    public void BackToGameTHeme()
+    {
+        SceneTransition transition = Instantiate(sceneTransitionPrefab);
+        transition.sceneName = "Start";
+    }
 }
