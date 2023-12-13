b2481c2230153d8440fcefb302a7c9d39a14de52
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2023-12-13 16:46:00 +0800

    reorganize prefabs and scripts
    start work on fire shaman animations

---
+++ Assets/Scripts/FireShaman/FireShamanEnemy.cs
@@ -0,0 +1,151 @@
+using System;
+using System.Linq;
+using UnityEngine;
+
+public class FireShamanEnemy : Enemy
+{
+    public float moveSpeed = 2;
+    public float acceleration = 40;
+    public float patrolRange = 3;
+    public float idleDuration = 1;
+    public float attackRange = 3;
+    public float attackCooldown = 1;
+    public bool doesSpecialAttack = false;
+    public int specialAttackCooldown = 5;
+    public Transform charmPoint;
+    public float groundCheckRadius;
+    public Fireball1EnemyAttack fireball1EnemyAttackPrefab;
+    //public FireTornadoEnemyAttack fireTornadoAttackPrefab;
+
+    internal Rigidbody2D rb;
+    internal Animator animator;
+    internal float homeX;
+    Player player;
+    float lastAttackTime = 0;
+    float attackCount = 0;
+    float idleTimer;
+    bool movingRight = true;
+    bool isIdle = false;
+    ContactFilter2D contactFilter;
+    readonly Collider2D[] overlaps = new Collider2D[16];
+
+    protected override void Awake()
+    {
+        base.Awake();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        contactFilter = new();
+        contactFilter.NoFilter();
+        contactFilter.useTriggers = false;
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        player = FindAnyObjectByType<Player>();
+        homeX = transform.position.x;
+        idleTimer = patrolRange;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        UpdateAttack();
+    }
+
+    void FixedUpdate()
+    {
+        UpdateMovement();
+    }
+
+    void UpdateMovement()
+    {
+        float currentVelocityX = rb.velocity.x;
+        float speed = Math.Abs(rb.velocity.x);
+        if (Vector2.Distance(transform.position, player.transform.position) > attackRange)
+        {
+            if (isIdle)
+            {
+                animator.SetFloat("Speed", speed > 1 ? currentVelocityX*0.5f : 0);
+                idleTimer -= Time.fixedDeltaTime;
+                if (idleTimer <= 0)
+                {
+                    isIdle = false;
+                }
+            }
+            else
+            {
+                float accelerationX = 0;
+                if (movingRight && transform.position.x-homeX < patrolRange)
+                {
+                    accelerationX = Math.Min(1, (moveSpeed-speed)*4)*acceleration;
+                    transform.rotation = Constants.rightRotation;
+                    animator.SetFloat("Speed", currentVelocityX < 0.5f ? 0.5f : speed);
+                }
+                else if (!movingRight && homeX-transform.position.x < patrolRange)
+                {
+                    accelerationX = -Math.Min(1, (moveSpeed-speed)*4)*acceleration;
+                    transform.rotation = Constants.leftRotation;
+                    animator.SetFloat("Speed", currentVelocityX > -0.5f ? 0.5f : speed);
+                }
+                else
+                {
+                    animator.SetFloat("Speed", speed > 1 ? currentVelocityX*0.5f : 0);
+                    idleTimer = idleDuration;
+                    isIdle = true;
+                    movingRight = !movingRight;
+                }
+                rb.AddForce(new(accelerationX*rb.mass, 0), ForceMode2D.Force);
+            }
+        }
+        else
+        {
+            animator.SetFloat("Speed", speed > 1 ? currentVelocityX*0.5f : 0);
+            movingRight = player.transform.position.x > transform.position.x;
+            transform.rotation = movingRight ? Constants.rightRotation : Constants.leftRotation;
+            idleTimer = idleDuration;
+            isIdle = true;
+        }
+    }
+
+    void UpdateAttack()
+    {
+        if (Vector2.Distance(transform.position, player.transform.position) <= attackRange)
+        {
+            if (Time.time >= lastAttackTime+attackCooldown)
+            {
+                lastAttackTime = Time.time;
+                if (doesSpecialAttack && attackCount >= specialAttackCooldown)
+                {
+                    animator.SetTrigger("Skill");
+                    attackCount = 0;
+                }
+                else if (CanFireballAttack())
+                {
+                    animator.SetTrigger("Attack");
+                    attackCount++;
+                }
+            }
+        }
+    }
+
+    bool CanFireballAttack()
+    {
+        if (groundCheckRadius > 0)
+        {
+            int count = Physics2D.OverlapCircle(charmPoint.position, transform.localScale.x*groundCheckRadius, contactFilter, overlaps); 
+            return overlaps.Take(count).All(c => !c.CompareTag("Ground"));
+        }
+        return true;
+    }
+
+    internal void PerformFireball1Attack()
+    {
+        Fireball1EnemyAttack attack = Instantiate(fireball1EnemyAttackPrefab, charmPoint.position, charmPoint.rotation);
+    }
+
+    internal void PerformLightningAttack()
+    {
+        //FireTornadoEnemyAttack attack = Instantiate(fireTornadoAttackPrefab, transform.position, transform.rotation);
+    }
+}
---
+++ Assets/Scripts/FireShaman/Fireball1EnemyAttack.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class Fireball1EnemyAttack : MonoBehaviour
+{
+    public float speed = 20;
+    public EnemyAttackTrigger explode;
+    public int damage;
+    Collider2D c2;
+    Rigidbody2D rb;
+    Animator animator;
+
+    void Awake()
+    {
+        c2 = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        explode.playerEnter = ApplyDamage;
+    }
+
+    void Start()
+    {
+        Vector2 facing = transform.right;
+        rb.velocity = facing*speed;
+    }
+
+    void TriggerExplode()
+    {
+        animator.SetTrigger("Explode");
+    }
+
+    internal void StartExplode()
+    {
+        c2.enabled = false;
+        rb.bodyType = RigidbodyType2D.Static;
+        explode.gameObject.SetActive(true);
+    }
+
+    internal void EndExplode()
+    {
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Invoke(nameof(TriggerExplode), 0.2f);
+    }
+
+    void ApplyDamage(EnemyAttackTrigger trigger, Player player)
+    {
+        Debug.Log("hit player");
+        player.TakeDamage(damage);
+    }
+}
