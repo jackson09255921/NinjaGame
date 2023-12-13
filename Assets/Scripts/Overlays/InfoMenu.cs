e1e603be82c0de306b93d60974acc27f41f5c813
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2023-12-14 02:21:44 +0800

    refine info menu navigation

--- Assets/Scripts/Overlays/InfoMenu.cs
+++ Assets/Scripts/Overlays/InfoMenu.cs
@@ -1,6 +1,7 @@
 
 
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class InfoMenu : MonoBehaviour
@@ -8,6 +9,7 @@ public class InfoMenu : MonoBehaviour
     public GameObject[] pages;
     public Button prevButton;
     public Button nextButton;
+    public Button exitButton;
     int page;
 
     internal int Page
@@ -22,12 +24,21 @@ public class InfoMenu : MonoBehaviour
                 pages[page].SetActive(true);
                 prevButton.interactable = page > 0;
                 nextButton.interactable = page < pages.Length-1;
+                if (!prevButton.interactable && EventSystem.current.currentSelectedGameObject == prevButton.gameObject)
+                {
+                    EventSystem.current.SetSelectedGameObject((nextButton.interactable ? nextButton : exitButton).gameObject);
+                }
+                if (!nextButton.interactable && EventSystem.current.currentSelectedGameObject == nextButton.gameObject)
+                {
+                    EventSystem.current.SetSelectedGameObject((prevButton.interactable ? prevButton : exitButton).gameObject);
+                }
             }
         }
     }
 
     void OnEnable()
     {
+        EventSystem.current.SetSelectedGameObject((pages.Length > 1 ? nextButton : exitButton).gameObject);
         Page = 0;
     }
 
